442aee2fd5d88a111e9a7b889ef53ee6852801ef
Sandeep <sandeep@mayking.in>
2025-01-03 20:36:04 +0530

    added php mailer setup

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -13,7 +13,6 @@
       event.preventDefault();
 
       let thisForm = this;
-
       let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
@@ -29,42 +28,53 @@
 
       if ( recaptcha ) {
         if(typeof grecaptcha !== "undefined" ) {
+          
           grecaptcha.ready(function() {
             try {
+              
               grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
               .then(token => {
                 formData.set('recaptcha-response', token);
                 php_email_form_submit(thisForm, action, formData);
               })
             } catch(error) {
+              
               displayError(thisForm, error);
             }
           });
         } else {
+         
           displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
         }
       } else {
+        
         php_email_form_submit(thisForm, action, formData);
       }
     });
   });
 
   function php_email_form_submit(thisForm, action, formData) {
+    
     fetch(action, {
       method: 'POST',
       body: formData,
       headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
     .then(response => {
+      
       if( response.ok ) {
+        
         return response.text();
       } else {
         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
       }
     })
     .then(data => {
+      
       thisForm.querySelector('.loading').classList.remove('d-block');
+      
       if (data.trim() == 'OK') {
+        
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
@@ -72,12 +82,15 @@
       }
     })
     .catch((error) => {
+      
       displayError(thisForm, error);
     });
   }
 
   function displayError(thisForm, error) {
+   
     thisForm.querySelector('.loading').classList.remove('d-block');
+    // thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
